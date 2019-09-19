b71161d85907157c96605199d856cd3f304860d7
Wendy Liang <wendy.liang@xilinx.com>
2019-09-19 00:17:47 -0700

    example: linux: update to use libfpga
    Update the linux example to use libfpga
    
    Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>

--- example/sys/linux/accel-bzip2.c
+++ example/sys/linux/accel-bzip2.c
@@ -1,17 +1,45 @@
 #include <acapd/accel.h>
+#include <errno.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-int main(void)
+
+void usage (const char *cmd)
 {
+	fprintf(stdout, "Usage %s -p <pkg_path>\n", cmd);
+}
+
+int main(int argc, char *argv[])
+{
+	int opt;
+	char *pkg_path = NULL;
 	acapd_accel_t bzip2_accel;
 
-	init_accel(&bzip2_accel);
+	while ((opt = getopt(argc, argv, "p:")) != -1) {
+		switch (opt) {
+		case 'p':
+			pkg_path = optarg;
+			break;
+		default:
+			usage(argv[0]);
+			return -EINVAL;
+		}
+	}
 
-	set_accel_pkg_img(&bzip2_accel, "system-bzip2.dtbo");
+	if (pkg_path == NULL) {
+		usage(argv[0]);
+		return -EINVAL;
+	}
+	printf("Initializing accel with %s.\n", pkg_path);
+	init_accel(&bzip2_accel, (acapd_accel_pkg_hd_t *)pkg_path);
 
+	printf("Loading accel %s.\n", pkg_path);
 	load_accel(&bzip2_accel, 0);
 
 	sleep(2);
+	printf("Removing accel %s.\n", pkg_path);
 	remove_accel(&bzip2_accel, 0);
 	return 0;
 }
