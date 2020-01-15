0d620688a298023489e22ba81a5955c7b58b1916
Wendy Liang <wendy.liang@xilinx.com>
2020-01-15 09:37:47 -0800

    Add helper header to define list type
    Add helper header to define list type.
    Use libmetal defined list structure today.
    
    Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>

---
+++ src/helper.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2020, Xilinx Inc. and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+/*
+ * @file	helper.h
+ * @brief	helper types functions definition.
+ */
+
+#ifndef _ACAPD_HELPER_H
+#define _ACAPD_HELPER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <metal/list.h>
+
+typedef struct metal_list acapd_list_t;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*  _ACAPD_HELPER_H */
