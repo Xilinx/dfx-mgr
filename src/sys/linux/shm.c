d22a4a299fd63277443bf6920212c2a1bee95e5e
Wendy Liang <wendy.liang@xilinx.com>
2020-01-21 21:57:03 -0800

    linux: shm: vfio: use shm->id as fd to allocate memory
    use shm->id as fd for memory allocation.
    
    Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>

--- src/sys/linux/shm.c
+++ src/sys/linux/shm.c
@@ -11,11 +11,23 @@ static void *acapd_vfio_alloc_shm(acapd_shm_allocator_t *allocator,
 	(void)allocator;
 	(void)attr;
 	if (shm == NULL) {
-		acapd_perror("%s: error, shm is NULL.\n", __func__);
+		acapd_perror("%s: shm is NULL.\n", __func__);
 		return NULL;
 	}
+	if (shm->id < 0) {
+		shm->id = 0;
+	}
 	shm->va = mmap(0, size, PROT_READ | PROT_WRITE,
-			MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+			MAP_PRIVATE | MAP_ANONYMOUS, shm->id, 0);
+
+	if (shm->va == NULL) {
+		acapd_perror("%s: failed to allocate memory from fd %d.\n",
+			     __func__, shm->id);
+	} else {
+		acapd_debug("%s: allocated memory %p from fd %d, size 0x%x.\n",
+			    __func__, shm->va,
+			    shm->id, size);
+	}
 	return shm->va;
 }
 
