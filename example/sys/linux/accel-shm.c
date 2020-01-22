f1fd00145586cd4d3c6f97f18dd6e1120890a2c2
Wendy Liang <wendy.liang@xilinx.com>
2020-01-21 22:05:36 -0800

    example: linux: shm: add more checking
    Add more checking to linux shared memory example.
    
    Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>

--- example/sys/linux/accel-shm.c
+++ example/sys/linux/accel-shm.c
@@ -5,6 +5,7 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 
@@ -21,6 +22,7 @@ int main(int argc, char *argv[])
 	acapd_shm_t tx_shm, rx_shm;
 	acapd_chnl_t chnls[2];
 	int ret;
+	void *tx_va, *rx_va;
 
 	while ((opt = getopt(argc, argv, "p:")) != -1) {
 		switch (opt) {
@@ -50,24 +52,44 @@ int main(int argc, char *argv[])
 	chnls[0].dir = ACAPD_DMA_DEV_W;
 	chnls[1].dev_name = "a4000000.dma";
 	chnls[1].ops = &axidma_vfio_dma_ops;
-	chnls[0].dir = ACAPD_DMA_DEV_R;
+	chnls[1].dir = ACAPD_DMA_DEV_R;
 	bzip2_accel.chnls = chnls;
 	bzip2_accel.num_chnls = 2;
 
 	/* allocate memory */
-	acapd_accel_alloc_shm(&bzip2_acccel, 1024*1024, &tx_shm);
-	acapd_accel_alloc_shm(&bzip2_acccel, 1024*1024, &rx_shm);
+	tx_va = acapd_accel_alloc_shm(&bzip2_accel, 1024*1024, &tx_shm);
+	if (tx_va == NULL) {
+		fprintf(stderr, "Failed to allocate tx memory.\n");
+		return -EINVAL;
+	}
+	rx_va = acapd_accel_alloc_shm(&bzip2_accel, 1024*1024, &rx_shm);
+	if (rx_va == NULL) {
+		fprintf(stderr, "Failed to allocate rx memory.\n");
+		return -EINVAL;
+	}
 
 	/* Transfer data */
 	ret = acapd_accel_write_data(&bzip2_accel, &tx_shm);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to write to accelerator.\n");
+		return -EINVAL;
+	}
 	/* TODO: Execute acceleration (optional as load_accel can also start
 	 * it from CDO */
 
 	/* Wait for output data ready */
 	/* For now, this function force to return 1 as no ready pin can poke */
 	ret = acapd_accel_wait_for_data_ready(&bzip2_accel);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to check if accelerator is ready.\n");
+		return -EINVAL;
+	}
 	/* Read data */
 	ret = acapd_accel_read_data(&bzip2_accel, &rx_shm);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to read from accelerator.\n");
+		return -EINVAL;
+	}
 
 	sleep(2);
 	printf("Removing accel %s.\n", pkg_path);
