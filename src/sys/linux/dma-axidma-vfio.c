0dcd5e2e6fe672a22f9bcce1068d023a8df82a1b
Tushal Javiya <tjaysukh@amd.com>
2026-06-23 21:42:29 -0700

    dfx-mgr: collapse multi-line register doc comments
    Put the multi-line /**< ... */ doc comments on the AXI DMA register
    offset and mask macros onto single lines, so clang-format's comment
    reflow produces clean output instead of misaligned, backslash-spliced
    comment blocks.
    
    No functional change.
    
    Signed-off-by: Tushal Javiya <tjaysukh@amd.com>
    
    Acked-by: Christian Kohn <chris.kohn@amd.com>

--- src/sys/linux/dma-axidma-vfio.c
+++ src/sys/linux/dma-axidma-vfio.c
@@ -22,20 +22,16 @@
 #include <unistd.h>
 #include "acapd-vfio-common.h"
 
-#define XAXIDMA_TX_OFFSET	0x00000000 /**< TX channel registers base
-					     *  offset */
-#define XAXIDMA_RX_OFFSET	0x00000030 /**< RX channel registers base
-					     * offset */
+#define XAXIDMA_TX_OFFSET	0x00000000 /**< TX channel registers base offset */
+#define XAXIDMA_RX_OFFSET	0x00000030 /**< RX channel registers base offset */
 #define XAXIDMA_CR_OFFSET	 0x00000000   /**< Channel control */
 #define XAXIDMA_SR_OFFSET	 0x00000004   /**< Status */
 #define XAXIDMA_CDESC_OFFSET	 0x00000008   /**< Current descriptor pointer */
 #define XAXIDMA_CDESC_MSB_OFFSET 0x0000000C   /**< Current descriptor pointer */
 #define XAXIDMA_TDESC_OFFSET	 0x00000010   /**< Tail descriptor pointer */
 #define XAXIDMA_TDESC_MSB_OFFSET 0x00000014   /**< Tail descriptor pointer */
-#define XAXIDMA_SRCADDR_OFFSET	 0x00000018   /**< Simple mode source address
-						pointer */
-#define XAXIDMA_SRCADDR_MSB_OFFSET	0x0000001C  /**< Simple mode source address
-						pointer */
+#define XAXIDMA_SRCADDR_OFFSET	 0x00000018   /**< Simple mode source address pointer */
+#define XAXIDMA_SRCADDR_MSB_OFFSET	0x0000001C  /**< Simple mode source address pointer */
 #define XAXIDMA_DESTADDR_OFFSET		0x00000018   /**< Simple mode destination address pointer */
 #define XAXIDMA_DESTADDR_MSB_OFFSET	0x0000001C   /**< Simple mode destination address pointer */
 #define XAXIDMA_BUFFLEN_OFFSET		0x00000028   /**< Tail descriptor pointer */
@@ -48,11 +44,9 @@
 
 #define XAXIDMA_HALTED_MASK		0x00000001  /**< DMA channel halted */
 #define XAXIDMA_IDLE_MASK		0x00000002  /**< DMA channel idle */
-#define XAXIDMA_ERR_INTERNAL_MASK	0x00000010  /**< Datamover internal
-						      *  err */
+#define XAXIDMA_ERR_INTERNAL_MASK	0x00000010  /**< Datamover internal err */
 #define XAXIDMA_ERR_SLAVE_MASK		0x00000020  /**< Datamover slave err */
-#define XAXIDMA_ERR_DECODE_MASK		0x00000040  /**< Datamover decode
-						      *  err */
+#define XAXIDMA_ERR_DECODE_MASK		0x00000040  /**< Datamover decode err */
 #define XAXIDMA_ERR_SG_INT_MASK		0x00000100  /**< SG internal err */
 #define XAXIDMA_ERR_SG_SLV_MASK		0x00000200  /**< SG slave err */
 #define XAXIDMA_ERR_SG_DEC_MASK		0x00000400  /**< SG decode err */
